f04a09b899d12f81357bee9114d5ab9d82e982bb
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2022-12-16 05:56:20 +0900

    feat: router 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Admin from './components/Admin';
+import Home from './components/Home';
+import './index.css';
+import CardPage from './pages/CardPage';
+import MyPage from './pages/MyPage';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/mypage', element: <MyPage /> },
+      { path: '/admin', element: <Admin /> },
+      { path: '/cards/:cardId', element: <CardPage /> },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ frontend/src/pages/CardPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CardPage() {
+  return <div>카드페이지</div>;
+}
